14a28ef8649c0e76c0472e97be616a02c29ceefd
Laden <delfanladen24@gmail.com>
2024-08-25 01:04:38 +0800

    create searching bar and filter button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,10 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { start } from "repl";
 import { Button } from "@/components/ui/button";
-import { ShoppingCart } from "lucide-react";
+import { ListFilter, ShoppingCart } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Badge } from "@/components/ui/badge";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 export default function Home() {
   return (
     <>
@@ -43,47 +46,67 @@ export default function Home() {
         </CarouselContent>
       </Carousel>
       <div className="flex flex-col py-4">
-        <h2 className="font-bold text-2xl">Pilih Menu</h2>
-        <Separator className="my-4" />
-        <h4 className="font-bold text-lg">Semua Menu</h4>
-        <Carousel className="w-full max-w-sm">
-          <CarouselContent className="-ml-1">
+        <h2 className="font-bold text-2xl mb-2">Pilih Menu</h2>
+        <div className="flex">
+          <Input
+            placeholder="Cari menu..."
+            className="h-12 w-full  lg:w-[250px] font-medium"
+          />
+          <Button variant="outline" className="h-12 ml-2">
+            <ListFilter size={24} />
+          </Button>
+        </div>
+        <ScrollArea className="w-full whitespace-nowrap rounded-md ">
+          <div className="flex py-4 gap-2">
             {Array.from({ length: 12 }).map((_, index) => (
-              <CarouselItem
+              <Button
                 key={index}
-                className="pl-1 basis-8/12 md:basis-1/2 lg:basis-4/12"
+                variant="outline"
+                className="flex items-center gap-1 p-2"
               >
-                <div className="p-1">
-                  <Card>
-                    <CardHeader className="text-lg font-semibold p-4">
-                      <Image
-                        src={"https://picsum.photos/200/300"}
-                        alt=""
-                        width={200}
-                        height={300}
-                      />
-                    </CardHeader>
-                    <CardContent className="flex flex-col items-start px-4 py-0">
-                      <span className="text-xl font-semibold">
-                        Ayam bakar {index + 1}
-                      </span>
-                      <span>Rp. 50.000,00</span>
-                    </CardContent>
-                    <CardFooter className="flex justify-between p-4">
-                      <Button
-                        variant={"outline"}
-                        className="w-full flex gap-2 text-blue-600 border-blue-600 "
-                      >
-                        <ShoppingCart size={16} />
-                        Tambah
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                </div>
-              </CarouselItem>
+                <span className="font-medium font-sans text-sm p-0">
+                  Menu {index + 1}
+                </span>
+              </Button>
             ))}
-          </CarouselContent>
-        </Carousel>
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+        <Separator className="my-2" />
+        <h4 className="font-bold text-lg text-gray-600">Rekomendasi</h4>
+        <Separator className="my-2" />
+        <div className="w-full grid grid-cols-2 gap-6">
+          {Array.from({ length: 12 }).map((_, index) => (
+            <Card key={index} className="border-0 bg-inherit shadow-none">
+              <CardHeader className="text-lg font-semibold p-0 rounded-sm shadow-none">
+                <Image
+                  src={"https://picsum.photos/200/300"}
+                  alt=""
+                  width={200}
+                  height={300}
+                  className="rounded-md"
+                />
+              </CardHeader>
+              <CardContent className="flex flex-col items-start p-0 py-2">
+                <span className="text-xs font-semibold text-foreground">
+                  Menu Rekomendasi {index + 1}
+                </span>
+                <span className="font-medium font-sans text-sm">
+                  Rp. 50.000,00
+                </span>
+              </CardContent>
+              <CardFooter className="flex justify-between w-full p-0">
+                <Button
+                  variant={"outline"}
+                  className="w-full flex gap-2 p-0 text-blue-600 border-blue-600 rounded-full"
+                >
+                  <ShoppingCart size={16} />
+                  <span className="font-bold">Tambah</span>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
       </div>
     </>
   );
