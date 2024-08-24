96aa1f06efb241bc4f19256ce03a609bee455d84
Laden <delfanladen24@gmail.com>
2024-08-24 23:00:43 +0800

    create navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,136 @@
+"use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { Menu, ShoppingCart } from "lucide-react";
+import {
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  PanelLeft,
+  ShoppingCart,
+  Users2,
+} from "lucide-react";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <>
-      <nav className="p-4 shadow-sm">
-        <div className="flex gap-2 justify-between">
-          <div className="flex gap-2 justify-center items-center">
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <header className="sticky top-0 z-30 flex justify-between h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+          {/* <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="overflow-hidden rounded-full"
+              >
+                <Image
+                  src="/placeholder-user.jpg"
+                  width={36}
+                  height={36}
+                  alt="Avatar"
+                  className="overflow-hidden rounded-full"
+                />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu> */}
+          <div className="flex items-center gap-2">
             <Image
               src="/logo.png"
-              alt="logo"
-              width={40}
-              height={40}
-              className="p-1 bg-black rounded-full"
+              width={26}
+              height={26}
+              alt="Logo"
+              className="bg-foreground rounded-full p-1"
             />
-            <span className="font-bold text-2xl text">Altri</span>
+            <span className="text-lg font-semibold italic font-sans">
+              Altri Coffe & Resto
+            </span>
           </div>
-          <div className="flex gap-4 items-center">
-            <ShoppingCart size={24} className=""/>
-            {/* <SignedOut>
-              <SignInButton />
-            </SignedOut>
+          <div className="flex items-center gap-2">
             <SignedIn>
               <UserButton />
-            </SignedIn> */}
-            <div className="flex items-center">
-              <Menu size={32} />
-            </div>
+            </SignedIn>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button size="icon" variant="outline" className="sm:hidden">
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle Menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="right" className="sm:max-w-xs">
+                <nav className="grid gap-6 text-lg font-medium">
+                  <Link
+                    href="#"
+                    className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+                  >
+                    <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+                    <span className="sr-only">Acme Inc</span>
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <Home className="h-5 w-5" />
+                    Dashboard
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <ShoppingCart className="h-5 w-5" />
+                    Orders
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-foreground"
+                  >
+                    <Package className="h-5 w-5" />
+                    Products
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <Users2 className="h-5 w-5" />
+                    Customers
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <LineChart className="h-5 w-5" />
+                    Settings
+                  </Link>
+                </nav>
+              </SheetContent>
+            </Sheet>
           </div>
-        </div>
-      </nav>
+        </header>
+      </div>
     </>
   );
 };
