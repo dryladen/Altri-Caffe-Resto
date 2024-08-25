2781d53c5e56ae209ceb36870443b0c75cc9be89
Laden <delfanladen24@gmail.com>
2024-08-25 17:47:22 +0800

    remove some package

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,9 @@ config({ path: '.env' });
 
 export default defineConfig({
   schema: './src/db/schema.ts',
-  out: './migrations',
-  dialect: 'sqlite',
-  driver: 'turso',
+  out: './supabase/migrations',
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: process.env.DATABASE_URL!,
   },
 });
