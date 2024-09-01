72689d3f6a076e6a450305fb4ea0b417889b0ffd
Laden <delfanladen24@gmail.com>
2024-09-01 15:25:40 +0800

    create admin architecture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 // import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,8 @@ export default function RootLayout({
     // <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
-        {/* <Navbar /> */}
         <main className="flex min-h-screen w-full flex-col bg-muted/40 relative">
-          {children}
+          <TooltipProvider>{children}</TooltipProvider>
         </main>
       </body>
     </html>
